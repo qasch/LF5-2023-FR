13745c8c63d9c9a8207196910a8bfa276a578735
Kai Schell <kai.schell@oscert.eu>
2023-10-06 11:54:09 +0200

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+# Ich bin ein Kommentar
+
+# Variablen und Datentypen
+
+# Integer
+ganze_zahl = 3
+
+
+# String
+zeichenkette = 'foo'
+zeichenkette_mit_doppelten_anfuehrungszeichen = "Geht das???"    # Umlaute besser vermeiden
+
+
+
+# Boolean / Wahrheitswert
+boolean = True
+print(boolean)
+
+datentyp = type(boolean)
+print(datentyp)
+
+
+# Variablen können unterschiedliche Datenytpen zugewiesen werden
+boolean = 987987
+print(boolean)
+
+print(type(boolean))
+# 1.: print(type(98787))
+# 2.: print("<class 'int'>")
+# 3.: -> Ausgabe
+
+
+boolean = 'Bin ich noch ein Boolean?'
+print(boolean)
+
+# Geht zwar, ist aber keine besonders gute Idee...
+true = False
+
+# Ausgabe
+#print(ganze_zahl)
+#print(zeichenkette)
+#print(boolean)
+#print(true)
