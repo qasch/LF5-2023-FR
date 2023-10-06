9bb8d4b7a70c7c66c2cf5746769c106b7055344c
Kai Schell <kai.schell@oscert.eu>
2023-10-06 13:43:09 +0200

    add simple_converter

---
+++ simple_converter.py
@@ -0,0 +1,16 @@
+# einfachen Umrechner: Tage in Stunden
+
+days = 3
+
+hours = days * 24
+
+addition = 4 + 3
+
+# print(days, hours)
+
+# mehrere Argumente durch Komma getrennt
+print(days, "Tage sind", hours, "Stunden")
+
+# Kombination von Variablen und Strings mit + Operator (Konkatenationsoperator)
+print(str(days) + " Tage sind " + str(hours) + " Stunden")
+
